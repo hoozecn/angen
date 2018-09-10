846080a20b044d0165e7d103daf2890bbbd5d848
Zhimin <hoo@econsulting.fr>
2018-09-10 23:11:14 +0800

    rename angen

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 # go get -u github.com/golang/dep/cmd/dep
 # dep ensure
 
-GOOS=windows GOARCH=386 go build -o angen.exe cmd/main.go
-GOOS=windows GOARCH=amd64 go build -o angen_64.exe cmd/main.go
-GOOS=darwin  GOARCH=amd64 go build -o angen_max cmd/main.go
+GOOS=windows GOARCH=386 go build -o angen.exe angen/main.go
+GOOS=windows GOARCH=amd64 go build -o angen_64.exe angen/main.go
+GOOS=darwin  GOARCH=amd64 go build -o angen_max angen/main.go
