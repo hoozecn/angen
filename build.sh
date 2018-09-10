00e4714bb0b6cb6042fed011ab977bc8059bb8b2
Zhimin <hoo@econsulting.fr>
2018-09-10 10:21:40 +0800

    remove packr

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 # comment out the following steps for the first build
 # go get -u github.com/golang/dep/cmd/dep
 # dep ensure
-# go get -u github.com/gobuffalo/packr/...
 
-GOOS=windows GOARCH=386 packr build -o generator.exe cmd/main.go
-GOOS=windows GOARCH=amd64 packr build -o generator_64.exe cmd/main.go
-packr build -o generator cmd/main.go
+GOOS=windows GOARCH=386 go build -o angen.exe cmd/main.go
+GOOS=windows GOARCH=amd64 go build -o angen_64.exe cmd/main.go
+GOOS=darwin  GOARCH=amd64 go build -o angen_max cmd/main.go
